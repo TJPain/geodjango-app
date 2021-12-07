122e17e072a95525074416a0e0979bc1c208dddb
Tom Pain <hello@tompa.in>
2021-12-07 20:40:04 +0000

    Connect to db
    Type: feat
    
    Connect to PostgreSQL db

--- shops/settings.py
+++ shops/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'shops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'shops',
+        'USER': 'shopadmin',
+        'PASSWORD': 'shoppass',
+        'HOST': 'localhost', 
+        'PORT': '5433',
     }
 }
 
