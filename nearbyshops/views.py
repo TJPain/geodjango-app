768de565c8ff735806adf6412e06fe86ed99e174
Tom Pain <hello@tompa.in>
2021-12-08 21:28:37 +0000

    Main view, urls, template
    Type: Feat
    
    Home page view, urls, basic test template

--- nearbyshops/views.py
+++ nearbyshops/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
 
-# Create your views here.
+latitude = 39.290440
+longitude = -76.612330
+
+user_location = Point(longitude, latitude, srid=4326)
+
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = "shops"
+    queryset = Shop.objects.annotate(
+        distance=Distance("location", user_location)
+    ).order_by("distance")[0:6]
+    template_name = "shops/index.html"
+
+home = Home.as_view()
